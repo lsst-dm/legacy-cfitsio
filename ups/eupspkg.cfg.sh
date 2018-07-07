0636625ab92481db63f4c63c4333a9cc09f96fbb
Kian-Tat Lim <ktl@slac.stanford.edu>
2018-07-06 19:22:51 -0500

    Upgrade to cfitsio3450; remove patches.

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -2,4 +2,7 @@
 
 NJOBS=1
 MAKE_BUILD_TARGETS="all shared fpack funpack"
+if [[ $(uname) != Darwin ]]; then
+	export LIBS=`curl-config --libs | sed -e s/-L/-Wl,-rpath-link=/ -e s/\ -lcurl//`
+fi
 CONFIGURE_OPTIONS="--prefix=$PREFIX --libdir=$PREFIX/lib"
